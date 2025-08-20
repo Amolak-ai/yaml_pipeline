27172a62a9684bbcd2067d89143e7c3c8a2160b8
Amol AK (EXT-Nokia) <amol.ak.ext@nokia.com>
2025-08-20 14:14:24 +0530

    rg3added

--- infra/main.tf
+++ infra/main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rg" {
   name     = "amak-rg"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "rg3" {
+  name     = "amak-rg3"
+  location = "West Europe"
+}
